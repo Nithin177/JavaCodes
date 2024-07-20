b1d14b70908589415d9ec2ed992e8c07fa81a26e
Nithin G <nithinshetty172001@gmail.com>
2024-07-20 13:49:11 +0530

    First Commit

--- ReverseRightHalfPyramid.java
+++ ReverseRightHalfPyramid.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class ReverseRightHalfPyramid {
+public class ReverseRightHalfPyramid {
+    public static void main(String[] args) {
+//        for (int i = 4; i > 0; i--) {
+//            for (int j = 1; j <= i; j++) {
+        
+                for (int i = 0; i < 4; i++) {
+                    for (int j = 1; (j+i) <= 4; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
